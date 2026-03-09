cb94a280d0bb86a16e2f8e6de5e4a0fc97f6caf2
arosler <43969584+arosler@users.noreply.github.com>
2026-03-09 01:13:03 -0400

    Update setup_grocy_please.sh

--- setup_grocy_please.sh
+++ setup_grocy_please.sh
@@ -14,7 +14,7 @@ systemctl enable nginx
 
 # Install the latest Grocy release
 cd /var/www/html
-wget -q https://github.com/grocy/grocy/releases/download/v4.5.0/grocy_4.5.0.zip
+wget -q https://releases.grocy.info/latest
 unzip -o latest -d /var/www/html && rm latest
 # Make everything compliant with Nginx standards
 chown -R www-data:www-data /var/www
@@ -24,13 +24,13 @@ sed -i "s/Setting('CURRENCY', 'USD');/Setting('CURRENCY', '$currency');/g" /var/
 
 # Set up NGINX with PHP & configuration files
 cd /etc/nginx/conf.d
-wget -q https://raw.githubusercontent.com/Tallyrald/grocy-install/main/fastcgi_params.conf
+wget -q https://raw.githubusercontent.com/arosler/grocy-install/main/fastcgi_params.conf
 mv fastcgi_params.conf fastcgi_params
-wget -q https://raw.githubusercontent.com/Tallyrald/grocy-install/main/nginx_grocy.conf
+wget -q https://raw.githubusercontent.com/arosler/grocy-install/main/nginx_grocy.conf
 sed -i "s/placeholder_domain/$domain/g" nginx_grocy.conf
 mv nginx_grocy.conf "$domain.conf"
 cd /etc/nginx
-wget -q https://raw.githubusercontent.com/Tallyrald/grocy-install/main/nginx_main_config.conf
+wget -q https://raw.githubusercontent.com/arosler/grocy-install/main/nginx_main_config.conf
 mv nginx.conf nginx.conf.original
 mv nginx_main_config.conf nginx.conf
 
